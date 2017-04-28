82b23544f055d69a7114299d7bc4b5e648519fac
Taylor Smith <taylor.smith@servicemaster.com>
2017-04-28 09:13:24 -0500

    try catch debug

--- lambda.js
+++ lambda.js
@@ -1,5 +1,9 @@
 'use strict'
+try {
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./dist/server')
 const server = awsServerlessExpress.createServer(app)
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+} catch(e) {
+    console.log(e);
+}
